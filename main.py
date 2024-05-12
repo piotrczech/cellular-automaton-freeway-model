5edd9da966a2b39a32fdb0da29cad3829b628bf2
Piotr Czech <czechpiotr2701@gmail.com>
2024-05-12 03:13:47 +0200

    feature: introduce cli arguments to use program

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
+import argparse, sys
 
 from lib.cellural_traffic_model import circle_freeway_model
 
+def define_parser():
+    parser = argparse.ArgumentParser(
+                    prog='A cellular automation model for freeway traffic',
+                    description="""Implementation of the cellular automaton model for freeway
+                        traffic discussed in the paper by Kai Nagel and Michael Schreckenberg
+                        (HAL Id: jpa-00246697).
+                    """,
+                    epilog='Text at the bottom of help')
+
+    parser.add_argument('-L', '--sites', default=70) 
+    parser.add_argument('-v', '--v_max', default=5) 
+    parser.add_argument('-d', '--density', default=0.25) 
+    parser.add_argument('-p', '--probability', default=0.4) 
+    parser.add_argument('-s', '--steps', default=50) 
+
+    return parser
+
 def main():
+    parser = define_parser()
+    args = parser.parse_args()
+
     road_history = circle_freeway_model.simulate_with_history(
-        L = 70,
-        v_max = 5,
-        density = 0.3,
-        p = 0.4,
-        steps = 40
+        L = args.sites,
+        v_max = args.v_max,
+        density = args.density,
+        p = args.probability,
+        steps = args.steps
     )
 
     for line in road_history:
